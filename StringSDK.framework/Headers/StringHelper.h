9c04d8fd5c9744d05b730fb148903d20b939bdcb
Xinlin Liu <apps_store@126.com>
2022-06-07 09:58:15 +0800

    Add files via upload

---
+++ StringSDK.framework/Headers/StringHelper.h
@@ -0,0 +1,18 @@
+//
+//  StringHelper.h
+//  StringSDK
+//
+//  Created by lxl on 2022/6/7.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StringHelper : NSObject
+
+- (NSString *)string;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ StringSDK.framework/Headers/StringSDK.h
@@ -0,0 +1,18 @@
+//
+//  StringSDK.h
+//  StringSDK
+//
+//  Created by lxl on 2022/6/7.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringSDK.
+FOUNDATION_EXPORT double StringSDKVersionNumber;
+
+//! Project version string for StringSDK.
+FOUNDATION_EXPORT const unsigned char StringSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringSDK/PublicHeader.h>
+#import <StringSDK/StringHelper.h>
+
